1630ef74dcb89b70165bd5ade836e3fcc08b2d3f
balo? <Bastian.preisel@hotmail.com>
2024-02-07 19:32:06 +0100

    cleaup

--- saveGameState.js
+++ saveGameState.js
@@ -5,8 +5,7 @@ function saveGameState() {
         url: window.location.href,
         players: globalThis.players
     };
-    window.localStorage.setItem('gameState', JSON.stringify(gameState));
-    // localStorage.setItem('gameState', JSON.stringify(gameState));
+    localStorage.setItem('gameState', JSON.stringify(gameState));
 }
 
 // Check if the user is leaving a match in progress
